63d0390dc53ddb1933e48698813272245c1d8ce7
Mohamed Ben Makhlouf <med.b.makhlouf@gmail.com>
2026-01-20 11:07:09 +0100

    fix: 🐛 enhance key filtering in directiveExtractor by using notNil utility for improved null safety

--- src/keys-builder/template/directive.extractor.ts
+++ src/keys-builder/template/directive.extractor.ts
@@ -26,7 +26,7 @@ import {
 } from './utils';
 import { coerceArray } from '../../utils/collection.utils';
 import { isConditionalExpression, isLiteralExpression, isLiteralMap } from '@jsverse/angular-utils';
-import { isString } from '../../utils/validators.utils';
+import { isString, notNil } from '../../utils/validators.utils';
 
 export function directiveExtractor(config: TemplateExtractorConfig) {
   const ast = parseTemplate(config);
@@ -98,7 +98,7 @@ function resolveKey(ast: OrArray<AST | string>): string[] {
         return resolveKey(expression.expression);
       }
     })
-    .filter((key) => key !== undefined && key !== null && (isString(key) || Array.isArray(key)))
+    .filter((key) => notNil(key) && (isString(key) || Array.isArray(key)))
     .flat();
 }
 
